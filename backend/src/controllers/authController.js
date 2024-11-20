4ef35f9c1e6f5df8ec579dde92426b2fd9e0d73a
Virang Jain <virangjain355@gmail.com>
2024-11-20 20:18:13 +0530

    Initial commit

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,9 @@
+const signup = (req, res) => {
+    res.send("signup Route");
+  }
+const login = (req, res) => {
+    res.send("login Route");
+  }
+const logout = (req, res) => {
+    res.send("logout Route");
+  }
